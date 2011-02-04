1febbd453ff8dc466d55c3f76e22e4cc6dcdf5ff
Enrico Sartori <sartori.enrico@gmail.com>
2011-02-04 09:43:40 +0100

    some errors in sql

--- sql/hotelboss.sql
+++ sql/hotelboss.sql
@@ -5,14 +5,13 @@ use `hotelboss`;
 
 
 create table if not exists `users` (
-`PrincipalID` text not null,
+`PrincipalID` varchar(50) not null,
 `Password` text not null,
-`Role` text not null,
+`Role` varchar(30) not null,
 `name` varchar(30) not null,
 `surname` varchar(30) not null,
 `email` varchar(100) not null,
 primary key (`PrincipalID`));
-);
 
 
 
